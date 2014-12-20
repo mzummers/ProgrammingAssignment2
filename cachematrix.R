33085aeafc0b87d00ee266144e74a5006cafd8b6
mzummers <mzummers@gmail.com>
2014-12-20 17:21:52 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Test only to figure out github
+## Assignment 2
+## cache matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    CacheMatrix<<- x
+    InverseMatrix<<- solve(x)
+    m<<- NULL
 }
 
-
-## Write a short comment describing this function
+## Test only to figure out github
+## Assignment 2
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        ## Test for martin
+  
+  ## Return a matrix that is the inverse of 'x'
+  
+      m<- solve(x)
+      
+      if (m = CacheMatrix) {
+          message("getting cached matrix")
+          return(InverseMatrix)
+      }
+      
+      cacheSolve<- m
+  
 }
