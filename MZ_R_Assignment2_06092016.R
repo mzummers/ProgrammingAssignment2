e54b11e0e1294c1c8789ad5479d99081503c1370
mzummers <mzummers@gmail.com>
2016-06-09 20:45:00 -0400

    Add files via upload

---
+++ MZ_R_Assignment2_06092016.R
@@ -0,0 +1,36 @@
+m1<-matrix(1:4,2,2)
+
+##
+## converting class example into class project
+## does not calculate the determinant of matrix to check if it can be inverted
+##
+
+## Make Matrix to assign values
+
+makeMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+## Caching will do actual inversion of matrix or lookup in cache
+
+cacheMatrix <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
+}
